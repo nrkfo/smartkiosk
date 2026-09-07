356e9ce1f92431c43398c684f7e367dc51243ec4
Nurbolat <nurbolat@nurbolat.local>
2026-09-07 11:27:52 +0500

    Redirect MainActivity to KioskActivity to support legacy Leanback Launcher shortcuts

--- app/src/main/java/com/smartkiosk/tv/MainActivity.kt
+++ app/src/main/java/com/smartkiosk/tv/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.smartkiosk.tv
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.FragmentActivity
+import com.smartkiosk.tv.ui.KioskActivity
 
-/**
- * Loads [MainFragment].
- */
 class MainActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                .replace(R.id.main_browse_fragment, MainFragment())
-                .commitNow()
+        val intent = Intent(this, KioskActivity::class.java).apply {
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP)
         }
+        startActivity(intent)
+        finish()
     }
 }
